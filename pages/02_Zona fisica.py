c8cd3f0ed312ef9648350c558c3383c5d866b1dd
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-07-25 23:30:16 +0200

    Update 02_Zona fisica.py

---
+++ pages/02_Zona fisica.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import altair as alt
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+
+st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
+st.title("Zona fisica")
+
+
+
+
+
+
+
+
+
+
+
+st.caption("Mattia Rampazzo - Vfc u19")
