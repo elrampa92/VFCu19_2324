f19b37f326e1cf16d528e103a2400169da2645c1
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-11-20 23:50:08 +0100

    Update 02_Zona fisica.py

--- pages/02_Zona fisica.py
+++ pages/02_Zona fisica.py
@@ -9,26 +9,43 @@ from pprint import pprint
 
 
 
-st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
-st.title("Zona fisica")
-
-
-import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
-import altair as alt
-import matplotlib.pyplot as plt
-from pprint import pprint
-
-from pages.Homepage.py import check_password
-
-if check_password():
-    st.write("#Page 1 👋")
 
 
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
 st.title("Zona fisica")
+
+import hmac
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+#fine login form
 
 
 '''RPE, Wellness = st.tabs(["RPE", "WELLNESS"])
@@ -39,21 +56,7 @@ with RPE:
 with Wellness:
 '''
 
-  
 
-
-
-st.caption("Mattia Rampazzo - Vfc u19")
-
-'''RPE, Wellness = st.tabs(["RPE", "WELLNESS"])
-
-with RPE:
-  
-
-with Wellness:
-'''
-
-  
 
 
 
