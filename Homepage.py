c269d2ea5d74eb9190e1af7191ea94f8df5b7000
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-11-20 23:29:17 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -7,6 +7,45 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 
 
+# streamlit_app.py
+
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# Main Streamlit app starts here
+st.write("Here goes your normal Streamlit app...")
+st.button("Click me")
+
+#fine login form
+
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
 st.title("Homepage")
 st.sidebar.success("Seleziona una pagina")
